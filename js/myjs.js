cca9a92c437151ffda15f12039cb760b90ad3c2d
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2019-03-06 23:13:22 -0600

    Adding checks for email and I Agree

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,27 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  function valEmail($email)
+  {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,8})?$/;
+    //tbh I don't know what this says, but stackoverflow says it's correct
+    return ( $email.length > 0 && emailReg.test($email));
+  }
+  $("#submit").on("click", function () {
+    if ($("#agreement").prop("checked"))
+      {
+        if (valEmail($("#email").val()))
+          {
+            alert("Welcome to Slavery!");
+          }
+        else
+          {
+            alert("Please input valid Email address!");
+          }
+      }
+    else
+      {
+        alert("You have not Agreed to the EULA!");
+      }
+  })
 });

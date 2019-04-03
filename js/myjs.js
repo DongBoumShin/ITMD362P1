486f3d6ed50107b2f1358d46a99b965ea801c126
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2019-04-03 18:00:14 -0500

    It spins backwards on larger screens now

--- js/myjs.js
+++ js/myjs.js
@@ -58,10 +58,35 @@ var degrees = 0;
 var speed = 50;
 function rotate() {
   var pic = document.getElementById('fun_to_spin');
+  if(window.innerWidth < 1024)
+    {rotateCoC(pic);}
+  else{rotateClk(pic)}
+}
+function rotateCoC(pic)
+{
   pic.style.transform = "rotate("+degrees+"deg)";
   degrees--;
   if(degrees<-359){
     degrees=0;
   }
 }
+function rotateClk(pic)
+{
+  pic.style.transform = "rotate("+degrees+"deg)";
+  degrees++;
+  if(degrees>359){
+    degrees=0;
+  }
+}
 window.onload=setInterval(rotate, 1000/speed);
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+addEvent(window, "resize", setInterval(rotate, 1000/speed));
